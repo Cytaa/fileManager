199d77c76ca72d39af82117a9ca6b1fb6a5c57bc
Piotr <piotrstachnio@gmail.com>
2019-11-19 00:56:10 +0100

    firsCommit

---
+++ cmdMenu.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+class CmdMenu:
+
+    def __init__(self):
+        print("""
+Welcome!
+This application will help u with menaging your pc.        
+        
+        
+        
+        """)
+
+
+
+    pass
+
+
+
+menu = CmdMenu()
---
+++ folder.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Folder:
+
+    def __init__(self):
+        pass
+
+    def getDir(self):
+        return  os.getcwd()
+    
+    def setDir(self, path):
+        pass
+
+
+folder = Folder()
