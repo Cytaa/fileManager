618ec6dd043162f37499914d352dee85d5519695
Piotr <piotrstachnio@gmail.com>
2019-11-22 23:40:32 +0100

    developing go to parent directory methode

--- folder.py
+++ folder.py
@@ -4,13 +4,39 @@ import os
 class Folder:
 
     def __init__(self):
-        pass
+        self.getDir()
 
     def getDir(self):
-        return  os.getcwd()
-    
+
+        self.dir = os.getcwd()
+
+        return dir
+
+    def findPar(self, path):
+
+        for i in range(len(path) - 1, -1, -1):
+            if path[i] == "\\":
+                return i
+
+
+    def getParDir(self, path, lastBack):
+
+        nDir = []
+
+        for i in range(lastBack):
+            nDir.append(path[i])
+
+        nDir = "".join(nDir)
+        
+        return nDir 
+
     def setDir(self, path):
+        os.chdir(path)
+
+    def goToParDir(self):
         pass
+        
 
 
 folder = Folder()
+folder.getParDir(folder.dir, folder.findPar(folder.dir))
