b8b91a801c1c2ad18b1689f441b85c1c114bdc72
Piotr <piotrstachnio@gmail.com>
2019-11-25 00:19:07 +0100

    further improvments

--- folder.py
+++ folder.py
@@ -26,6 +26,18 @@ class Folder:
 
         return nDir
 
+    def prepPathToChild(self, dirPath):#doesnt work yet need
+
+        nDir = []
+
+        for i in range(len(self.dir) - 1, len(self.dir) - 1 + len(dirPath), 1):
+            if i == len(self.dir):
+                self.dir[i] = "//"
+            else:
+                self.dir[i] = dirPath[i - len(self.dir) - 1]
+
+        return self.dir
+
     def setDir(self, path):
         os.chdir(path)
         self.dir = path
@@ -33,22 +45,22 @@ class Folder:
     def goToParDir(self):
         self.setDir(self.prepPathToPar())
 
+    def goToChilDir(self,dirPath):#not working yet
+        self.setDir(self.prepPathToChild(dirPath))
+
+
 
     def showMeDir(self):
         root = self.dir
 
         for item in os.listdir(root):
-            if os.path.isfile(os.path.join(root,item)):
+            if os.path.isfile(os.path.join(root, item)):
                 print(item + "\n")
-            elif os.path.isdir(os.path.join(root,item)):
+            elif os.path.isdir(os.path.join(root, item)):
                 print(item + "\n")
-
-
-
 
 
 folder = Folder()
-print(folder.getDir())
 folder.goToParDir()
 print(folder.getDir())
-folder.showMeDir()
+
