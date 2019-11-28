dbf719c8b7ae65547fc7fb37fbcb74bf74d30a8c
Piotr <piotrstachnio@gmail.com>
2019-11-28 23:35:20 +0100

    creating a simple command line menu

--- cmdMenu.py
+++ cmdMenu.py
@@ -7,16 +7,24 @@ from folder import Folder
 class CmdMenu:
 
     def __init__(self):
-        print("""   
-1.Show whats in dir
-2.Go to par dir
-3.Go to child dir
-        """)
-        chooser = input("Whats your option?:")
-
+        self.chooser = 0
         folder = Folder()
 
-    def nextOption(self, chooser):
+        while self.chooser != 9:
+            print(""" 
+1. Show your current directory
+2. Show whats in current directory
+2. Go to parent directory
+3. Go to child directory""")
+            self.chooser = int(input("choose ur option"))
+            if self.chooser == 1:
+                print(folder.getDir())
+            elif self.chooser == 2:
+                folder.showMeDir()
+
+        
+
+    def nextOption(self):
         pass
 
 
