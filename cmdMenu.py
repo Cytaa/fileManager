92762fce2bb096e6e73a822cbdacfa40e34a6260
Piotr <piotrstachnio@gmail.com>
2019-11-29 11:51:34 +0100

    Further improvement

--- cmdMenu.py
+++ cmdMenu.py
@@ -11,27 +11,24 @@ class CmdMenu:
 
     def createAMenu(self):
         while self.chooser != 9:
+            print(self.folder.getDir())
             print(""" 
-1. Show your current directory
-2. Show whats in current directory
-3. Go to parent directory
-4. Go to child directory
-5. Create new dir""")
-            self.chooser = int(input("choose ur option: "))
+1. Show whats in current directory
+2. Go to parent directory
+3. Go to child directory
+4. Create new dir""")
+            self.chooser = int(input("choose your option"))
             if self.chooser == 1:
                 self.clear()
-                print(self.folder.getDir())
-            elif self.chooser == 2:
-                self.clear()
                 self.folder.showMeDir()
-            elif self.chooser == 3:
+            elif self.chooser == 2:
                 self.folder.goToParDir()
                 self.clear()
-            elif self.chooser == 4:
+            elif self.chooser == 3:
                 dir = input("insert dir name: ")
                 self.folder.goToChilDir(dir)
                 self.clear()
-            elif self.chooser == 5:
+            elif self.chooser == 4:
                 name = input("whats ur dirs name: ")
                 self.folder.createADir(name)
                 self.clear()
